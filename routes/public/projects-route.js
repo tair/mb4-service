8db9145a42ce581b7009c8dffff239db21666f73
trilok2000 <trilok.prithvi@gmail.com>
2022-10-28 15:35:40 -0700

    MB4-57: Index page of all the published projects, browse by project title

--- routes/public/projects-route.js
+++ routes/public/projects-route.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import * as projectsController from '../../controllers/projects-controller.js';
-import * as mediaController from '../../controllers/media-controller.js';
-import * as dataDumpController from '../../controllers/datadump-controller.js';
+import express from 'express'
+import * as projectsController from '../../controllers/projects-controller.js'
+import * as mediaController from '../../controllers/media-controller.js'
+import * as dataDumpController from '../../controllers/datadump-controller.js'
 
 const projectsRouter = express.Router()
 
@@ -9,6 +9,7 @@ projectsRouter.get('/data_dump', dataDumpController.dataDump)
 
 projectsRouter.get('/', projectsController.getProjects)
 projectsRouter.get('/:id', projectsController.getProjectsById)
+projectsRouter.get('/titles/:sort_by', projectsController.getProjectTitles)
 projectsRouter.get('/:id/media', mediaController.getMediaFiles)
 
-export default projectsRouter;
+export default projectsRouter
