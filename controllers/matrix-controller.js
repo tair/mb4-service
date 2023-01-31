cce58475e7b44a6fedad1fc835a63b948c5b736e
Kenzley <kenzley@users.noreply.github.com>
2023-01-30 20:46:41 -0500

    Implement Nexus, NeXML, and TNT exports (#40)

--- controllers/matrix-controller.js
+++ controllers/matrix-controller.js
@@ -1,3 +1,7 @@
+import { ExportOptions } from '../lib/matrix-export/exporter.js'
+import { NexusExporter } from '../lib/matrix-export/nexus-exporter.js'
+import { NeXMLExporter } from '../lib/matrix-export/nexml-exporter.js'
+import { TNTExporter } from '../lib/matrix-export/tnt-exporter.js'
 import * as matrixService from '../services/matrix-service.js'
 import * as partitionService from '../services/partition-service.js'
 
@@ -32,33 +36,55 @@ export async function getMatrices(req, res) {
 }
 
 export async function download(req, res) {
+  const matrixId = parseInt(req.params.matrixId)
+  if (!matrixId) {
+    res.status(400).json({ message: 'The request must be contain a matrix ID.' })
+    return
+  }
+
   let fileExtension
+  let exporter
+  const options = new ExportOptions()
   switch (req.query.format) {
     case 'tnt':
       fileExtension = 'tnt'
+      exporter = new TNTExporter((txt) => res.write(txt))
       break
     case 'nexml':
       fileExtension = 'xml'
+      exporter = new NeXMLExporter((txt) => res.write(txt))
       break
     case 'nexus':
     default:
       fileExtension = 'nex'
+      options.blocks = await matrixService.getMatrixBlocks(matrixId)
+      exporter = new NexusExporter((txt) => res.write(txt))
       break
   }
 
   const date = new Date()
-  const matrixId = req.params.matrixId
   const filename = `mbank_X${matrixId}_${date.getFullYear()}-${
     date.getMonth() + 1
   }-${date.getDate()}-${('0' + date.getUTCHours()).slice(-2)}${(
     '0' + date.getUTCMinutes()
   ).slice(-2)}.${fileExtension}`
   res.set({
-    'Content-Type': 'application/octet-stream',
-    'Content-Disposition': 'filename=' + filename,
+    'Content-Type': 'text/plain; charset=utf-8',
+    'Content-Disposition': 'attachment; filename=' + filename,
     'Cache-Control': 'private',
     'Last-Modified': new Date(),
-    Pragma: 'no-store',
+    'Pragma': 'no-store',
   })
-  res.status(200).send('Hello')
+  res.status(200)
+
+  options.matrix = await matrixService.getMatrix(matrixId)
+  options.taxa = await matrixService.getTaxaInMatrix(matrixId)
+  options.characters = await matrixService.getCharactersInMatrix(matrixId)
+  options.cellsTable = await matrixService.getCells(matrixId)
+  options.includeNotes = !!req.query.notes
+  options.cellNotes = options.includeNotes
+    ? await matrixService.getCellNotes(matrixId)
+    : null
+  exporter.export(options)
+  res.end()
 }
--- services/matrix-service.js
+++ services/matrix-service.js
@@ -1,6 +1,12 @@
 import sequelizeConn from '../util/db.js'
+import { Table } from '../util/table.js'
+import { models } from '../models/init-models.js'
 
-async function getMatrices(projectId) {
+export async function getMatrix(matrixId) {
+  return models.Matrix.findByPk(matrixId)
+}
+
+export async function getMatrices(projectId) {
   const [rows] = await sequelizeConn.query(
     `
       SELECT matrix_id, user_id, title, type
@@ -11,7 +17,7 @@ async function getMatrices(projectId) {
   return rows
 }
 
-async function getCounts(matrixIds) {
+export async function getCounts(matrixIds) {
   if (matrixIds.length == 0) {
     return {}
   }
@@ -55,11 +61,11 @@ async function getCounts(matrixIds) {
 
   const [characterRulesCount] = await sequelizeConn.query(
     `
-				SELECT mco.matrix_id, count(*) c
-				FROM character_rules cr
-				INNER JOIN matrix_character_order AS mco 
+        SELECT mco.matrix_id, count(*) c
+        FROM character_rules cr
+        INNER JOIN matrix_character_order AS mco
             ON mco.character_id = cr.character_id
-				WHERE mco.matrix_id IN (?)
+        WHERE mco.matrix_id IN (?)
         GROUP BY mco.matrix_id`,
     { replacements: [matrixIds] }
   )
@@ -130,4 +136,179 @@ async function getCounts(matrixIds) {
   }
 }
 
-export { getCounts, getMatrices }
+export async function getTaxaInMatrix(matrixId, partitionId = undefined) {
+  const replacements = [matrixId]
+  let join = ''
+  let clause = ''
+  if (partitionId) {
+    join += 'INNER JOIN taxa_x_partitions AS txp ON txp.taxon_id = t.taxon_id'
+    clause += 'AND txp.partition_id = ?'
+    replacements.push(partitionId)
+  }
+  const [rows] = await sequelizeConn.query(
+    `
+      SELECT DISTINCT
+        t.*, mto.position
+      FROM taxa t
+      INNER JOIN matrix_taxa_order AS mto ON mto.taxon_id = t.taxon_id
+      INNER JOIN matrices AS m ON m.matrix_id = mto.matrix_id
+      ${join}
+      WHERE
+        m.matrix_id = ?
+        ${clause}
+      ORDER BY mto.position`,
+    { replacements }
+  )
+
+  return rows
+}
+
+export async function getCharactersInMatrix(matrixId, partitionId = undefined) {
+  const replacements = [matrixId]
+  let join = ''
+  let clause = ''
+  if (partitionId) {
+    join = `INNER JOIN characters_x_partitions AS cxp
+        ON cxp.character_id = c.character_id`
+    clause = 'AND cxp.partition_id = ?'
+    replacements.push(partitionId)
+  }
+  const [characterRows] = await sequelizeConn.query(
+    `
+      SELECT
+        c.character_id, c.name, c.ordering, c.type, c.description, mco.notes
+      FROM characters AS c
+      INNER JOIN matrix_character_order AS mco
+        ON mco.character_id = c.character_id
+      ${join}
+      WHERE
+        mco.matrix_id = ?
+        ${clause}
+      ORDER BY mco.position`,
+    { replacements: [replacements] }
+  )
+
+  const [stateRows] = await sequelizeConn.query(
+    `
+      SELECT cs.character_id, cs.state_id, cs.name, cs.num, cs.description
+      FROM characters c
+      INNER JOIN matrix_character_order AS mco
+        ON mco.character_id = c.character_id
+      INNER JOIN character_states AS cs
+        ON cs.character_id = c.character_id
+      ${join}
+      WHERE
+        mco.matrix_id = ?
+        ${clause}
+      ORDER BY cs.character_id, cs.num`,
+    { replacements }
+  )
+  const statesMap = new Map()
+  for (const row of stateRows) {
+    const characterId = parseInt(row.character_id)
+    if (!statesMap.has(characterId)) {
+      statesMap.set(characterId, [])
+    }
+    statesMap.get(characterId).push(row)
+  }
+
+  for (const row of characterRows) {
+    const characterId = parseInt(row.character_id)
+    const states = statesMap.get(characterId)
+    row.states = states
+  }
+
+  return characterRows
+}
+
+export async function getCells(matrixId, partitionId = undefined) {
+  const replacements = [matrixId]
+  let join = ''
+  let clause = ''
+  if (partitionId) {
+    join =
+      `
+      INNER JOIN characters_x_partitions AS cxp
+        ON cxp.character_id = c.character_id AND cxp.partition_id = ?
+      INNER JOIN taxa_x_partitions AS txp
+        ON txp.character_id = c.taxon_id AND txp.partition_id = cxp.partition_id
+      `
+    clause = 'AND p.partition_id = ?'
+    replacements.push(partitionId)
+  }
+
+  const [rows] = await sequelizeConn.query(
+    `
+      SELECT
+        c.cell_id, c.taxon_id, c.character_id, c.state_id, c.is_npa,
+        c.is_uncertain, c.start_value, c.end_value
+      FROM cells c
+      INNER JOIN matrix_character_order AS mco
+        ON mco.character_id = c.character_id AND mco.matrix_id = c.matrix_id
+      INNER JOIN matrix_taxa_order AS mto
+        ON mto.taxon_id = c.taxon_id AND mto.matrix_id = c.matrix_id
+      ${join}
+      WHERE
+        c.matrix_id = ?
+        ${clause}
+      ORDER BY c.taxon_id, c.character_id, c.state_id`,
+    { replacements: replacements }
+  )
+  const cells = new Table()
+  for (const row of rows) {
+    const taxonId = parseInt(row.taxon_id)
+    const characterId = parseInt(row.character_id)
+    if (!cells.has(taxonId, characterId)) {
+      cells.set(taxonId, characterId, [])
+    }
+    cells.get(taxonId, characterId).push(row)
+  }
+  return cells
+}
+
+export async function getCellNotes(matrixId, partitionId = undefined) {
+  const replacements = [matrixId]
+  let join = ''
+  let clause = ''
+  if (partitionId) {
+    join =
+      `
+      INNER JOIN characters_x_partitions AS cxp
+        ON cxp.character_id = c.character_id AND cxp.partition_id = ?
+      INNER JOIN taxa_x_partitions AS txp
+        ON txp.character_id = c.taxon_id AND txp.partition_id = cxp.partition_id
+      `
+    clause = 'AND p.partition_id = ?'
+    replacements.push(partitionId)
+  }
+
+  const [rows] = await sequelizeConn.query(
+    `
+      SELECT
+        c.taxon_id, c.character_id, c.notes
+      FROM cell_notes c
+      INNER JOIN matrix_character_order AS mco
+        ON mco.character_id = c.character_id AND mco.matrix_id = c.matrix_id
+      INNER JOIN matrix_taxa_order AS mto
+        ON mto.taxon_id = c.taxon_id AND mto.matrix_id = c.matrix_id
+      ${join}
+      WHERE
+        c.matrix_id = ?
+        ${clause}
+      ORDER BY mto.position, mco.position`,
+    { replacements: replacements }
+  )
+  return rows
+}
+
+export async function getMatrixBlocks(matrixId) {
+  const [rows] = await sequelizeConn.query(
+    `
+      SELECT name, content
+      FROM matrix_additional_blocks
+      WHERE matrix_id = ?
+      ORDER BY name, block_id`,
+    { replacements: [matrixId] }
+  )
+  return rows
+}
