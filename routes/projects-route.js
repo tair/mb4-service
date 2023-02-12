1c751b1740e4eaaeb8f542217bd76ef32aa99077
Kenzley <kenzley@users.noreply.github.com>
2023-02-11 22:31:55 -0500

    Implement Taxa and Character fetching endpoints (#45)

--- routes/projects-route.js
+++ routes/projects-route.js
@@ -1,9 +1,17 @@
 import express from 'express'
 import matrixRouter from './matrix-route.js'
+import characterRouter from './characters-route.js'
+import taxaRouter from './taxa-route.js'
 import { maybeAuthenticateToken } from '../controllers/auth-controller.js'
 
 const projectRouter = express.Router({ mergeParams: true })
 
+projectRouter.use(
+  '/:projectId/characters',
+  maybeAuthenticateToken,
+  characterRouter
+)
 projectRouter.use('/:projectId/matrices', maybeAuthenticateToken, matrixRouter)
+projectRouter.use('/:projectId/taxa', maybeAuthenticateToken, taxaRouter)
 
 export default projectRouter
