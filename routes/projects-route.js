232b5af8bc004adb94715c9f2ad5f7fe2a8bfd3c
Kenzley Alphonse <kenzley@users.noreply.github.com>
2023-04-10 18:32:49 -0400

    Merged documents

--- routes/projects-route.js
+++ routes/projects-route.js
@@ -11,8 +11,8 @@ import { authenticateToken, maybeAuthenticateToken } from '../controllers/auth-c
 const projectRouter = express.Router({ mergeParams: true })
 
 projectRouter.use('/:projectId/characters', authenticateToken, characterRouter)
-projectRouter.use('/:projectId/matrices/:matrixId/edit', maybeAuthenticateToken, matrixEditorRouter)
 projectRouter.use('/:projectId/documents', authenticateToken, documentRouter)
+projectRouter.use('/:projectId/matrices/:matrixId/edit', maybeAuthenticateToken, matrixEditorRouter)
 projectRouter.use('/:projectId/matrices', authenticateToken, matrixRouter)
 projectRouter.use('/:projectId/taxa', authenticateToken, taxaRouter)
 
