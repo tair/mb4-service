8be0c437e39d411096f88d7c7739427f945f9cc0
Kenzley Alphonse <kenzley@users.noreply.github.com>
2023-12-03 14:43:16 -0500

    Add the ability to generate project overview stats via the task queue.

--- routes/tasks-route.js
+++ routes/tasks-route.js
@@ -5,5 +5,6 @@ const router = express.Router()
 
 router.get('/process', controller.process)
 router.get('/reset', controller.reset)
+router.get('/cron', controller.runCronJobs)
 
 export default router
