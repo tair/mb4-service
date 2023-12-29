b970485f6ae36bc152366093e5dbd78de45dbfc0
aquintanilla1 <alvaro.quintanilla@stonybrook.edu>
2023-12-28 21:04:33 -0500

    Added test file for Partition Project Publisher.

---
+++ tests/lib/partition-project-publisher.test.js
@@ -0,0 +1,9 @@
+import { describe, expect, test } from '@jest/globals'
+
+import { PartitionProjectPublisher } from '../../lib/PartitionProjectPublisher'
+import { PartitionPublishHandler } from '../../lib/task-handlers/partition-publish-handler'
+import { models } from '../../models/init-models'
+
+describe('PartitionProjectPublisherTests', () => {
+    
+})
