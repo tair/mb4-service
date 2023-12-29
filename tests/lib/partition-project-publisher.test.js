81842185ce3eaa251d4c9d925725280f36cea1cc
aquintanilla1 <alvaro.quintanilla@stonybrook.edu>
2023-12-28 21:21:07 -0500

    Moved and renamed partition project publisher file.

--- tests/lib/partition-project-publisher.test.js
+++ tests/lib/partition-project-publisher.test.js
@@ -1,6 +1,6 @@
 import { describe, expect, test } from '@jest/globals'
 
-import { PartitionProjectPublisher } from '../../lib/PartitionProjectPublisher'
+import { PartitionProjectPublisher } from '../../src/lib/partition-project-publisher'
 import { PartitionPublishHandler } from '../../lib/task-handlers/partition-publish-handler'
 import { models } from '../../models/init-models'
 
