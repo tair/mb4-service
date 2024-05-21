f266622e2468f27610b2e24aa9b4f847d8b98221
William Barnett <will892@LAPTOP-GCQMH64J>
2024-05-21 11:57:10 -0700

    Further changes for naming conventions

--- src/routes/projects-route.js
+++ src/routes/projects-route.js
@@ -3,7 +3,6 @@ import express from 'express'
 import bibliographyRouter from './bibliography-route.js'
 import characterRouter from './characters-route.js'
 import documentRouter from './document-route.js'
-import duplicationRouter from './duplication-route.js'
 import foliosRouter from './folios-route.js'
 import institutionRouter from './institution-route.js'
 import matrixRouter from './matrix-route.js'
@@ -33,7 +32,6 @@ projectRouter.use(authorizeProject)
 projectRouter.use('/bibliography', bibliographyRouter)
 projectRouter.use('/characters', characterRouter)
 projectRouter.use('/documents', documentRouter)
-projectRouter.use('/duplication/request', duplicationRouter)
 projectRouter.use('/folios', foliosRouter)
 projectRouter.use('/institutions', institutionRouter)
 projectRouter.use('/matrices', matrixRouter)
