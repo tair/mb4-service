c156797c0746693c9641ae8a030bded50fd23fe1
Will-78 <148902613+Will-78@users.noreply.github.com>
2024-05-23 23:20:21 -0700

    Institutions controller (#115)
    * Updated to match new changes made in March
    
    * new functions to controller and api connections
    
    * Edits to make a functioning assign and remove feature
    
    * Edits to make /create, update to service to also obtain the id along with the name, and reverted schema.
    
    * git reverted following files: schema.sql, package, and package-lock .json
    
    * renamed variables and functions to be more accurate
    
    * Further changes for naming conventions
    
    ---------
    
    Co-authored-by: William Barnett <will892@LAPTOP-GCQMH64J>

---
+++ src/controllers/institution-controller.js
@@ -0,0 +1,107 @@
+import * as institutionService from '../services/institution-service.js'
+import { models } from '../models/init-models.js'
+import { Sequelize } from 'sequelize'
+import sequelizeConn from '../util/db.js'
+
+export async function fetchProjectInstitutions(req, res) {
+  try {
+    const institutions = await institutionService.fetchInstitutions(
+      req.params.projectId
+    )
+    res.status(200).json({ institutions })
+  } catch (e) {
+    console.error('Problem while getting project institutions (controller).', e)
+    res
+      .status(500)
+      .json({ message: 'Error while getting project institutions.' })
+  }
+}
+
+export async function addInstitutionToProject(req, res) {
+  const projectId = req.params.projectId
+  const institutionId = req.body.institutionId
+
+  if (institutionId == null) {
+    res.status(404).json({ message: 'Institution is not found' })
+    return
+  }
+
+  try {
+    const projectInstitution = models.InstitutionsXProject.build({
+      project_id: projectId,
+      institution_id: institutionId,
+    })
+
+    const transaction = await sequelizeConn.transaction()
+    await projectInstitution.save({
+      transaction,
+      user: req.user,
+    })
+
+    await transaction.commit()
+    const institution = await models.Institution.findByPk(institutionId)
+    res.status(200).json({ institution })
+  } catch (e) {
+    console.error(e)
+    res.status(500).json({ message: 'could not assign the two' })
+  }
+}
+
+export async function removeInstitutionFromProject(req, res) {
+  const projectId = req.params.projectId
+  const institutionIds = req.body.institutionIds
+
+  if (institutionIds == null) {
+    res.status(404).json({ message: 'Institutions not found' })
+    return
+  }
+
+  const transaction = await sequelizeConn.transaction()
+  try {
+    await models.InstitutionsXProject.destroy({
+      where: {
+        project_id: projectId,
+        institution_id: institutionIds,
+      },
+      transaction: transaction,
+      individualHooks: true,
+      user: req.user,
+    })
+
+    await transaction.commit()
+    res.status(200).json({ institutionIds })
+  } catch (e) {
+    await transaction.rollback()
+    res.status(500).json({ message: 'could not remove association' })
+    console.log('error removing association', e)
+  }
+}
+
+export async function searchInstitutions(req, res) {
+  const searchTerm = req.query.searchTerm
+  const projectId = req.params.projectId
+
+  try {
+    const projectInstitutions = await models.InstitutionsXProject.findAll({
+      attributes: ['institution_id'],
+      where: { project_id: projectId },
+    })
+
+    // extract ids because sequelize expects values not objects
+    const dupes = projectInstitutions.map((i) => i.institution_id)
+
+    // get all institutions with like name segment and not within other model
+    const institutions = await models.Institution.findAll({
+      attributes: ['institution_id', 'name'],
+      where: {
+        name: { [Sequelize.Op.like]: '%' + searchTerm + '%' },
+        institution_id: { [Sequelize.Op.notIn]: dupes },
+      },
+    })
+
+    return res.status(200).json(institutions)
+  } catch (e) {
+    res.status(500).json({ message: 'error obtaining list of insititutions' })
+    console.log('error obtaining list of insititutions', e)
+  }
+}
