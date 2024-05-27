aff2187b1eec2e34504bda7cabd2d9553f2e1159
Kenzley Alphonse <kenzley@users.noreply.github.com>
2024-05-27 16:56:11 -0400

    Create a Emailer and a Email Task Handler so that we can send emails more effectively.

--- src/lib/task-handlers/init-handlers.js
+++ src/lib/task-handlers/init-handlers.js
@@ -1,15 +1,20 @@
 import { DOICreationHandler } from './doi-creation-handler.js'
+import { EmailHandler } from './email-handler.js'
 import { FileDeletionHandler } from './file-deletion-handler.js'
 import { PartitionPublishHandler } from './partition-publish-handler.js'
 import { ProjectDuplicationHandler } from './project-duplication-handler.js'
 import { ProjectOverviewGenerationHandler } from './project-overview-generation-handler.js'
 
+/**
+ * Registers a TaskHandler to be processed when the /task endpoint is invoked.
+ */
 function registerTaskHandler(handler) {
   handlers.set(handler.getName(), handler)
 }
 
 export const handlers = new Map()
 registerTaskHandler(new DOICreationHandler())
+registerTaskHandler(new EmailHandler())
 registerTaskHandler(new FileDeletionHandler())
 registerTaskHandler(new PartitionPublishHandler())
 registerTaskHandler(new ProjectDuplicationHandler())
