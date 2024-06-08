dbdee196eb94aafd5bef8b3df55d4fd0427150dd
joearias1121 <joearias1121@gmail.com>
2024-06-08 18:14:18 -0400

    making the members be listed alphabetically

--- src/services/members-service.js
+++ src/services/members-service.js
@@ -15,7 +15,8 @@ export async function getMembersInProject(projectId) {
   const [rows] = await sequelizeConn.query(
     `SELECT user_id, project_id, fname, lname, administrator, member_email, 
     member_role, member_name 
-    FROM member_stats WHERE project_id = ?`,
+    FROM member_stats WHERE project_id = ?
+    ORDER BY member_name`,
     { replacements: [projectId] }
   )
   return rows
