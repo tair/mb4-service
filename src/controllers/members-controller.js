28af7f14ab7e5d482923645bd3612cdbcc5dcffb
joearias1121 <joearias1121@gmail.com>
2024-06-13 22:43:40 -0400

    adding ability to delete a member in the controller

--- src/controllers/members-controller.js
+++ src/controllers/members-controller.js
@@ -1,3 +1,5 @@
+import sequelizeConn from '../util/db.js'
+import { models } from '../models/init-models.js'
 import * as service from '../services/members-service.js'
 
 // this returns the members for specified project
@@ -12,6 +14,21 @@ export async function getMembers(req, res) {
         console.error(`Error: Cannot fetch members for ${projectId}`, err)
         res.status(500).json({ message: 'Error while fetching members.' })
       }
+}
+
+export async function deleteMember(req, res) {
+  const link_ids = req.body.link_ids
+  const transaction = await sequelizeConn.transaction()
+  await models.ProjectDocument.destroy({
+    where: {
+      link_id: link_ids,
+    },
+    transaction: transaction,
+    individualHooks: true,
+    user: req.user,
+  })
+  await transaction.commit()
+  res.status(200).json({ link_ids: link_ids })
 }
 
 //converts member data from db into its own object
