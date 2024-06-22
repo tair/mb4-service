122df8b3eecefab93c641214ee5858af2ea5695f
joearias1121 <joearias1121@gmail.com>
2024-06-22 15:19:31 -0400

    deleted a couple files that were unnecessary

--- src/routes/member-route.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-import * as controller from '../controllers/members-controller.js'
-
-const memberRouter = express.Router({ mergeParams: true })
-
-memberRouter.get('/', controller.getMembers)
-
-export default memberRouter
