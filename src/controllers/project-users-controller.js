2a48eec24c83cfbf3862a7307ca180ff08a1f61c
JoeArias1121 <96800288+JoeArias1121@users.noreply.github.com>
2024-06-25 02:36:03 -0400

    Delete member (#132)
    * created membersController folder
    
    * began setting up the backend to get members in a project
    
    * completed backend side to fetch members
    
    * debugging of previous commit
    
    * making the members be listed alphabetically
    
    * creating member model
    
    * changed the table that data is being retrieved from
    
    * deleted member model
    
    * small fixes
    
    * small fix after running linter
    
    * adding ability to delete a member in the controller
    
    * began adding delete method to user controller and consolidating project_user and member backend
    
    * added a more practical "isAdmin" to pass as data to frontend and finished consolidating the similar member service and controller into project-user
    
    * finished backend part for deleting member
    
    * ran npm run pretty and lint
    
    * deleted a couple files that were unnecessary
    
    * consolidated service and now I fetch admin with help from project intervept to avoid uneccessary call to data base
    
    * combined similar get methods for project-users-controller

--- src/controllers/project-users-controller.js
+++ src/controllers/project-users-controller.js
@@ -1,7 +1,45 @@
 import * as service from '../services/project-user-service.js'
+import sequelizeConn from '../util/db.js'
+import { models } from '../models/init-models.js'
 
 export async function getProjectUsers(req, res) {
-  const projectId = req.project.project_id
-  const users = await service.getUsersInProjects([projectId])
-  res.status(200).json({ users })
+  const projectId = req.params.projectId
+  const admin = req.project.user_id
+  try {
+    const users = await service.getUsersInProjects(projectId)
+    res.status(200).json({
+      users: users.map((row) => convertUser(row, admin)),
+    })
+  } catch (err) {
+    console.error(`Error: Cannot fetch members for ${projectId}`, err)
+    res.status(500).json({ message: 'Error while fetching members.' })
+  }
+}
+
+export async function deleteUser(req, res) {
+  const link_id = req.body.link_id
+  const transaction = await sequelizeConn.transaction()
+  await models.ProjectsXUser.destroy({
+    where: {
+      link_id: link_id,
+    },
+    transaction: transaction,
+    individualHooks: true,
+    user: req.user,
+  })
+  await transaction.commit()
+  res.status(200).json({ link_id: link_id })
+}
+
+//converts member data from db into its own object
+function convertUser(row, admin) {
+  return {
+    user_id: parseInt(row.user_id),
+    link_id: parseInt(row.link_id),
+    admin: row.user_id == admin,
+    fname: row.fname,
+    lname: row.lname,
+    membership_type: parseInt(row.membership_type),
+    email: row.email,
+  }
 }
