ba01379c7e470785844623fe39626de5309f29f6
joearias1121 <joearias1121@gmail.com>
2024-07-16 17:53:15 -0400

    started and completed back end for deleting member groups then ran pretty

--- src/routes/project-member-groups-route.js
+++ src/routes/project-member-groups-route.js
@@ -4,5 +4,6 @@ import * as controller from '../controllers/project-member-groups-controller.js'
 const projectMemberGroupsRouter = express.Router({ mergeParams: true })
 
 projectMemberGroupsRouter.get('/', controller.getProjectGroups)
+projectMemberGroupsRouter.post('/delete', controller.deleteGroup)
 
 export default projectMemberGroupsRouter
