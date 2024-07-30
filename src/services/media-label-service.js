e02e684a7c0d2c52a8201edda4662639d34fefb1
Kenzley Alphonse <kenzley@users.noreply.github.com>
2024-07-29 20:27:51 -0400

    Remove unneeded joins for media labels

--- src/services/media-label-service.js
+++ src/services/media-label-service.js
@@ -2,12 +2,7 @@ import sequelizeConn from '../util/db.js'
 
 export async function getMediaLabels(mediaId, tableNumber) {
   const [labels] = await sequelizeConn.query(
-    `
-    SELECT *
-    FROM media_labels l
-    INNER JOIN ca_users AS wu ON wu.user_id = l.user_id
-    INNER JOIN media_files AS m ON m.media_id = l.media_id
-    WHERE l.media_id = ? AND l.table_num = ?`,
+    'SELECT * FROM media_labels WHERE media_id = ? AND table_num = ?',
     {
       replacements: [mediaId, tableNumber],
     }
