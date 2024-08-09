80d569af26b0f965a9043788994e6e36c0633b14
MsXN31 <xingguo.chen@arabidopsis.org>
2024-08-08 19:41:38 -0700

    Dev release 20240808 (#144)
    * MB4-63: Update media specimen to have 1-1 relationship with taxa.
    
    * MB4-64: Centralize taxa and specimen naming methods.
    
    * MB4-66: Implement the specimen details dump of the published project
    
    * MB4-68: Implement taxa dump for published projects.
    
    * Prettify files.

--- src/services/specimen-service.js
+++ src/services/specimen-service.js
@@ -1,4 +1,11 @@
 import sequelizeConn from '../util/db.js'
+import User from '../models/user.js'
+import BibliographicReference from '../models/bibliographic-reference.js'
+import { getSpecimenNameForPublishedProject } from '../util/specimen.js'
+import {
+  getSpecimenTaxonNameForPublishedProject,
+  getSpecimenTaxaSortFieldValues,
+} from '../util/taxa.js'
 
 export async function getProjectSpecimens(projectId) {
   const [rows] = await sequelizeConn.query(
@@ -36,19 +43,135 @@ export async function getVoucheredSpecimenIdByTaxaIds(projectId, taxaIds) {
   return map
 }
 
+// for published project dumping
 export async function getSpecimenDetails(projectId) {
   const [rows] = await sequelizeConn.query(
     `
-      SELECT s.reference_source, s.institution_code,s.user_id,
-          s.collection_code, s.catalog_number, s.created_on, u.fname, u.lname,
-          t.*
+      SELECT s.specimen_id, s.reference_source, s.institution_code, s.description,
+          s.collection_code, s.catalog_number, s.created_on, t.*, u.fname, u.lname
       FROM specimens s
-      INNER JOIN taxa_x_specimens ts ON s.specimen_id = ts.specimen_id
-      INNER JOIN taxa t ON t.taxon_id = ts.taxon_id
-      INNER JOIN ca_users u ON u.user_id = s.user_id AND s.project_id = ?`,
+      LEFT JOIN ca_users u ON u.user_id = s.user_id
+      LEFT JOIN taxa_x_specimens ts ON s.specimen_id = ts.specimen_id
+      LEFT JOIN taxa t ON t.taxon_id = ts.taxon_id
+      WHERE s.project_id = ?`,
     { replacements: [projectId] }
   )
-  return rows
+  const bibRefMap = await getBibliographicReferencesMap(projectId)
+  const hitMap = await getPublishedHitsMap(projectId)
+  // biblicoreferences
+  return rows.map((r) => {
+    const obj = {
+      reference_source: getReferenceSourceText(r.reference_source),
+      institution_code: r.institution_code,
+      collection_code: r.collection_code,
+      catalog_number: r.catalog_number,
+      created_on: r.created_on,
+      user_name: User.getName(r.fname, r.lname),
+      specimen_name: getSpecimenNameForPublishedProject(r),
+      taxon_name: getSpecimenTaxonNameForPublishedProject(r),
+      sort_fields: getSpecimenTaxaSortFieldValues(r),
+    }
+    if (hitMap[r.specimen_id]) {
+      obj['hits'] = r.specimen_id
+    }
+    if (r.description) {
+      obj['specimen_notes'] = r.description.trim()
+    }
+    let referenceTexts = bibRefMap[r.specimen_id]
+    if (referenceTexts) {
+      obj['references'] = referenceTexts
+    }
+    return obj
+  })
+}
+
+// for published project dumping
+export async function getUnidentifiedSpecimenDetails(projectId) {
+  const [rows] = await sequelizeConn.query(
+    `
+      SELECT s.specimen_id, s.reference_source, s.institution_code, s.description,
+          s.collection_code, s.catalog_number, s.created_on, u.fname, u.lname
+      FROM specimens s
+      LEFT JOIN ca_users u ON u.user_id = s.user_id
+      LEFT JOIN taxa_x_specimens ts ON s.specimen_id = ts.specimen_id
+      WHERE ts.specimen_id is null AND s.project_id = ?
+      ORDER BY s.reference_source DESC, s.institution_code, s.collection_code, s.catalog_number;`,
+    { replacements: [projectId] }
+  )
+  const bibRefMap = await getBibliographicReferencesMap(projectId)
+  const hitMap = await getPublishedHitsMap(projectId)
+  // biblicoreferences
+  return rows.map((r) => {
+    const obj = {
+      reference_source: getReferenceSourceText(r.reference_source),
+      institution_code: r.institution_code,
+      collection_code: r.collection_code,
+      catalog_number: r.catalog_number,
+      created_on: r.created_on,
+      user_name: User.getName(r.fname, r.lname),
+      specimen_name: getSpecimenNameForPublishedProject(r),
+    }
+    if (hitMap[r.specimen_id]) {
+      obj['hits'] = r.specimen_id
+    }
+    if (r.description) {
+      obj['specimen_notes'] = r.description.trim()
+    }
+    let referenceTexts = bibRefMap[r.specimen_id]
+    if (referenceTexts) {
+      obj['references'] = referenceTexts
+    }
+    return obj
+  })
+}
+
+function getReferenceSourceText(refSource) {
+  return refSource ? 'Unvouchered' : 'Vouchered'
+}
+
+async function getBibliographicReferencesMap(projectId) {
+  const [references] = await sequelizeConn.query(
+    `
+      SELECT s.specimen_id, br.*
+      FROM specimens s
+      INNER JOIN specimens_x_bibliographic_references AS sxbr ON sxbr.specimen_id = s.specimen_id
+      INNER JOIN bibliographic_references br ON sxbr.reference_id = br.reference_id
+      WHERE s.project_id = ?
+    `,
+    { replacements: [projectId] }
+  )
+
+  let referenceMap = {}
+  for (let i = 0; i < references.length; i++) {
+    let ref = references[i]
+    if (!referenceMap[ref.specimen_id]) {
+      referenceMap[ref.specimen_id] = []
+    }
+    referenceMap[ref.specimen_id].push(
+      BibliographicReference.getCitationText(ref, null)
+    )
+  }
+  return referenceMap
+}
+
+async function getPublishedHitsMap(projectId) {
+  const [hits] = await sequelizeConn.query(
+    `
+      SELECT row_id, count(*) as count
+      FROM specimens s
+      INNER JOIN stats_pub_hit_log h ON h.row_id = s.specimen_id AND h.project_id = s.project_id
+      WHERE s.project_id = ?
+      AND h.hit_type = 'S'
+      GROUP BY s.project_id, h.row_id
+    `,
+    { replacements: [projectId] }
+  )
+  let hitMap = {}
+  for (let i = 0; i < hits.length; i++) {
+    let hit = hits[i]
+    hitMap[hit.row_id] = hit.count
+  }
+  return hitMap
 }
 
 export async function isSpecimensInProject(specimenIds, projectId) {
