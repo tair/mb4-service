560350de20acd1a17f621988e5abaa8be00a4522
joearias1121 <joearias1121@gmail.com>
2024-08-25 23:10:48 -0400

    able to add a member to a project as long as they are a valid morphobank user

--- src/controllers/project-users-controller.js
+++ src/controllers/project-users-controller.js
@@ -121,9 +121,8 @@ export async function createUser(req, res) {
   const values = req.body.json
   const transaction = await sequelizeConn.transaction()
   try {
-    const user = await models.User.findOne({
+    const user = await models.User.findByPk(values.user_id, {
       attributes:['fname', 'lname', 'email', 'user_id'],
-      where: { email: values.email },
     })
     const project_x_user = models.ProjectsXUser.build({
       project_id: projectId,
@@ -147,8 +146,7 @@ export async function createUser(req, res) {
         membership_type: project_x_user.membership_type,
         link_id: project_x_user.link_id,
       },
-      req.project.user_id,
-      []
+      req.project.user_id
     )})
   } catch (err) {
     await transaction.rollback()
@@ -157,43 +155,20 @@ export async function createUser(req, res) {
   }
 
 }
-
-export async function checkEmail(req, res) {
+// check to see if the user is active and if not deleted
+export async function isAvailable(req, res) {
   const email = req.body.email
   try {
     const user = await models.User.findOne({
-      attributes:['fname', 'lname', 'email', 'user_id'],
+      attributes:['fname', 'lname', 'email', 'user_id', 'active', 'userclass'],
       where: { email: email },
     })
-    // sending back a response but with data in desired structure
     res.status(200).json({
       exist: user ? true: false,
       user: user ? user: {email: email},
-    })
-  } catch (err) {
-    console.error(`Error: Error while fetching user for ${email}`, err)
-    res.status(500).json({ message: 'Error while fetching user.' })
-  }
-}
-
-export async function inProject(req, res) {
-  const email = req.body.email
-  const projectId = req.project.project_id
-  try {
-    const user = await models.User.findOne({
-      attributes:['user_id'],
-      where: { email: email },
-    })
-
-    const projectXUser = await models.ProjectsXUser.findOne({
-      attributes: ['link_id'],
-      where: {
-        project_id: projectId,
-        user_id: user.user_id,
-      },
-    })
-    res.status(200).json({
-      inProject: projectXUser ? true: false,
+      errorMessage: user.active == 0 ? "Was not able to add user as a member because they're inactive":
+      user.userclass == 255 ? "Was not able to add user as a member because they have been deleted":
+      false,
     })
   } catch (err) {
     console.error(`Error: Error while fetching user for ${email}`, err)
@@ -202,7 +177,7 @@ export async function inProject(req, res) {
 }
 
 //converts member data from db into its own object
-function convertUser(row, admin, groupIds) {
+function convertUser(row, admin, groupIds = []) {
   return {
     user_id: parseInt(row.user_id),
     link_id: parseInt(row.link_id),
--- src/routes/project-users-route.js
+++ src/routes/project-users-route.js
@@ -6,8 +6,7 @@ const projectUsersRouter = express.Router({ mergeParams: true })
 projectUsersRouter.get('/', controller.getProjectUsers)
 projectUsersRouter.post('/create', controller.createUser)
 projectUsersRouter.post('/delete', controller.deleteUser)
-projectUsersRouter.post('/check/email', controller.checkEmail)
-projectUsersRouter.post('/in/project', controller.inProject)
+projectUsersRouter.post('/is/available', controller.isAvailable)
 projectUsersRouter.post('/:linkId/edit', controller.editUser)
 
 export default projectUsersRouter
