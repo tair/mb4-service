55f9bd911305de768ac443e7a4e5f8a70f029c2b
joearias1121 <joearias1121@gmail.com>
2024-08-26 23:34:22 -0400

    renaming

--- src/controllers/project-users-controller.js
+++ src/controllers/project-users-controller.js
@@ -124,15 +124,10 @@ export async function createUser(req, res) {
     const user = await models.User.findByPk(values.user_id, {
       attributes: ['fname', 'lname', 'email', 'user_id'],
     })
-    const project_x_user = models.ProjectsXUser.build({
+    const project_x_user = models.ProjectsXUser.create({
       project_id: projectId,
       user_id: user.user_id,
       membership_type: values.membership_type,
-    })
-
-    await project_x_user.save({
-      transaction,
-      user: req.user,
     })
 
     await transaction.commit()
@@ -157,7 +152,7 @@ export async function createUser(req, res) {
   }
 }
 // check to see if the user is active and if not deleted
-export async function isAvailable(req, res) {
+export async function isEmailAvailable(req, res) {
   const email = req.body.email
   try {
     const user = await models.User.findOne({
--- src/routes/project-users-route.js
+++ src/routes/project-users-route.js
@@ -6,7 +6,7 @@ const projectUsersRouter = express.Router({ mergeParams: true })
 projectUsersRouter.get('/', controller.getProjectUsers)
 projectUsersRouter.post('/create', controller.createUser)
 projectUsersRouter.post('/delete', controller.deleteUser)
-projectUsersRouter.post('/is/available', controller.isAvailable)
+projectUsersRouter.post('/isEmailAvailable', controller.isEmailAvailable)
 projectUsersRouter.post('/:linkId/edit', controller.editUser)
 
 export default projectUsersRouter
