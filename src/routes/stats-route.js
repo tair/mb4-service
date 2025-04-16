635ad5b79a152f92c689b856afa7b36d8b445e15
Kartik <kartik.khosa@phxbio.org>
2025-04-16 14:51:13 -0500

    small fixes

--- src/routes/stats-route.js
+++ src/routes/stats-route.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import { getStatsController } from '../controllers/stats-controller.js';
 import { getHomeStatsController } from '../controllers/home-stats-controller.js';
 
 const router = express.Router();
@@ -9,9 +8,6 @@ router.use((req, res, next) => {
   console.log('Stats route hit:', req.method, req.url);
   next();
 });
-
-// Get all statistics
-router.get('/', getStatsController);
 
 // Home stats routes
 router.get('/home', getHomeStatsController);
