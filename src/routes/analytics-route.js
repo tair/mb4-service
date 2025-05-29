8e353a2c7314c0c90846d18b9a4df3ea2cc307e5
kartikphoenix <116028287+kartikphoenix@users.noreply.github.com>
2025-05-29 13:21:19 -0500

    Analytics Initial framework (#178)

---
+++ src/routes/analytics-route.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import * as analyticsController from '../controllers/analytics-controller.js'
+import { authenticateToken } from './auth-interceptor.js'
+
+const analyticsRouter = express.Router()
+
+analyticsRouter.post('/view', authenticateToken, analyticsController.logProjectView)
+analyticsRouter.post('/download', authenticateToken, analyticsController.logDownload)
+
+// For testing so no auth is required
+// analyticsRouter.post('/view', analyticsController.logProjectView)
+// analyticsRouter.post('/download', analyticsController.logDownload)
+
+export default analyticsRouter 
