a92860673fc9687157491e78368281dc659ddcdf
swapp <swapp19902@gmail.com>
2025-08-19 12:10:58 -0700

    added domain env var

--- src/lib/task-handlers/doi-creation-handler.js
+++ src/lib/task-handlers/doi-creation-handler.js
@@ -4,7 +4,7 @@ import { Handler, HandlerErrors } from './handler.js'
 import { QueryTypes } from 'sequelize'
 import { models } from '../../models/init-models.js'
 
-const BASE_URL = process.env.FRONTEND_URL
+const BASE_URL = `https://${process.env.DOMAIN}/project`
 
 /** A handler to creating DOIs for Projects and Matrices. */
 export class DOICreationHandler extends Handler {
