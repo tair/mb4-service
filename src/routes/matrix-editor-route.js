2ee28fa8b829aea48a43905c36f2a8bed5a2150a
Kartik <kartik.khosa@phxbio.org>
2025-09-26 19:22:42 -0500

    role based membership added

--- src/routes/matrix-editor-route.js
+++ src/routes/matrix-editor-route.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import * as controller from '../controllers/matrix-editor-controller.js'
+import { requireEntityEditPermission, EntityType } from '../lib/auth-middleware.js'
 
 const router = express.Router({ mergeParams: true })
+
+// Apply matrix edit permission to all matrix editor routes
+router.use(requireEntityEditPermission(EntityType.MATRIX))
 
 // Taxa endpoints
 router.post('/getAvailableTaxa', controller.getAvailableTaxa)
--- src/routes/specimens-route.js
+++ src/routes/specimens-route.js
@@ -1,23 +1,25 @@
 import express from 'express'
 import * as controller from '../controllers/specimens-controller.js'
+import { requireEntityEditPermission, EntityType } from '../lib/auth-middleware.js'
 
 const specimenRouter = express.Router({ mergeParams: true })
 
 specimenRouter.get('/', controller.getSpecimens)
-specimenRouter.post('/create', controller.createSpecimen)
-specimenRouter.post('/create/batch', controller.createSpecimens)
-specimenRouter.post('/delete', controller.deleteSpecimens)
+specimenRouter.post('/create', requireEntityEditPermission(EntityType.SPECIMEN), controller.createSpecimen)
+specimenRouter.post('/create/batch', requireEntityEditPermission(EntityType.SPECIMEN), controller.createSpecimens)
+specimenRouter.post('/delete', requireEntityEditPermission(EntityType.SPECIMEN), controller.deleteSpecimens)
 specimenRouter.post('/usages', controller.getUsage)
 specimenRouter.post('/search', controller.search)
 
-specimenRouter.post('/:specimenId/edit', controller.editSpecimen)
+specimenRouter.post('/:specimenId/edit', requireEntityEditPermission(EntityType.SPECIMEN), controller.editSpecimen)
 
 specimenRouter.get('/:specimenId/citations', controller.getCitations)
-specimenRouter.post('/:specimenId/citations/create', controller.createCitation)
+specimenRouter.post('/:specimenId/citations/create', requireEntityEditPermission(EntityType.SPECIMEN), controller.createCitation)
 specimenRouter.post(
   '/:specimenId/citations/:citationId/edit',
+  requireEntityEditPermission(EntityType.SPECIMEN),
   controller.editCitation
 )
-specimenRouter.post('/:specimenId/citations/delete', controller.deleteCitations)
+specimenRouter.post('/:specimenId/citations/delete', requireEntityEditPermission(EntityType.SPECIMEN), controller.deleteCitations)
 
 export default specimenRouter
