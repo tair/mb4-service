b2d2e7423b0c546cbc23c5cfeac56bf98e54fdd9
Kartik <kartik.khosa@phxbio.org>
2026-02-10 14:16:49 -0600

    Missing tables in duplication that are causing an error

--- src/services/project-duplication-service.js
+++ src/services/project-duplication-service.js
@@ -294,6 +294,8 @@ const DUPLICATED_TABLES = [
   models.MatrixAdditionalBlock,
   models.BibliographicAuthor,
   models.MediaLabel,
+  models.CompositeTaxon,
+  models.CompositeTaxonSource,
 ]
 
 const IGNORED_TABLES = [
