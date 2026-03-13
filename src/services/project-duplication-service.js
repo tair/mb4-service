c970b008adf73056e9606c5294963c363f89633e
kartikphoenix <116028287+kartikphoenix@users.noreply.github.com>
2026-03-13 11:41:06 -0500

    Revert "Add hp_featured_projects to project duplication allowlist"

--- src/services/project-duplication-service.js
+++ src/services/project-duplication-service.js
@@ -262,7 +262,6 @@ const DUPLICATED_TABLES = [
   models.MediaFile,
   models.Matrix,
   models.MatrixImage,
-  models.FeaturedProject,
   models.CharacterOrdering,
   models.Character,
   models.Taxon,
